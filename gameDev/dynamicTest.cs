4a9420c2dfdebeda139b0e204b3c56d2cc5d5796
Liam Sparling <liam.sparling@ieee.org>
2021-12-04 17:41:25 -0600

    Actually added the scaling blocks

--- gameDev/dynamicTest.cs
+++ gameDev/dynamicTest.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-public class dynamicTest : MonoBehaviour
+public class DynamicTest : MonoBehaviour
 {
     private Obstacle[] obstacles = new Obstacle[6];
 
@@ -8,19 +8,40 @@ public class dynamicTest : MonoBehaviour
     private double newBlockTime;
     public float newBlockCreationDelay = 5;
     //private int curState;
-    public float dist = 0;
+    public float dist = 20;
+    Modes gameMode = Modes.Endless;
+    private int[] standardGame;
+    private int curObstacle;
+    public float obstacleSpeed = 10f;
+    private float relMaxSpeed;
+    private float relNewBlock;
+    Points myPoints;
 
     void Start()
     {
         Vector3 offset = transform.position + new Vector3(0, 0, dist);
-        obstacles[0] = new ObstacleLeft(offset);
-        obstacles[1] = new ObstacleVLeft(offset);
-        obstacles[2] = new ObstacleRight(offset);
-        obstacles[3] = new ObstacleVRight(offset);
-        obstacles[4] = new ObstacleDown(offset);
-        obstacles[5] = new ObstacleUp(offset);
+        Vector3 scale = prefab.transform.localScale;
+        obstacles[0] = new ObstacleLeft(offset, scale);
+        obstacles[1] = new ObstacleVLeft(offset, scale);
+        obstacles[2] = new ObstacleRight(offset, scale);
+        obstacles[3] = new ObstacleVRight(offset, scale);
+        obstacles[4] = new ObstacleDown(offset, scale);
+        obstacles[5] = new ObstacleUp(offset, scale);
 
         newBlockTime = 0;
+
+        standardGame = new int[] 
+        { 
+            1, 2, 3, 4, 5, 0,
+            3, 4, 5, 0, 3, 5, 5, 4, 1, 3,
+            3, 3, 1, 1, 2, 0, 5, 4, 3, 5, 1, 4
+        };
+        curObstacle = 0;
+
+        myPoints = GameObject.Find("Points").GetComponent<Points>();
+
+        relMaxSpeed = obstacleSpeed * 0.1f;
+        relNewBlock = 0.025f;
         //Vector3 pos = transform.position + new Vector3(x, y, z);
 
         //For the move left obstacle
@@ -30,12 +51,33 @@ public class dynamicTest : MonoBehaviour
 
     void Update()
     {
-        Vector3[] curPos;
         newBlockTime -= Time.deltaTime;
-        int rInt = (int)Random.Range(0, obstacles.Length - 0.001f);
         if (newBlockTime <= 0)
         {
-            curPos = obstacles[rInt].positions;
+            Vector3[] curPos;
+            if (gameMode == Modes.Standard)
+            {
+                if (curObstacle >= standardGame.Length)
+                {
+                    curObstacle++;
+                    if (curObstacle >= standardGame.Length + 4)
+                    {
+                        //trigger game end here
+                        print("Game Done");
+                    }
+                    return;
+                }
+                curPos = standardMode();
+            }
+            else if (gameMode == Modes.Endless)
+            { 
+                curPos = endlessMode();
+            }
+            else
+            {
+                curPos = obstacles[0].positions;
+                print("error");
+            }
 
             for (int i = 0; i < curPos.Length; i++)
             {
@@ -44,6 +86,36 @@ public class dynamicTest : MonoBehaviour
             newBlockTime = newBlockCreationDelay;
         }
     }
+
+    private Vector3[] standardMode()
+    {
+        SpeedUp();
+        SpeedUp();
+        return obstacles[standardGame[curObstacle++]].positions;
+    } 
+
+    private Vector3[] endlessMode()
+    {
+        SpeedUp();
+        int rInt = (int)Random.Range(0, obstacles.Length - 0.001f);
+        return obstacles[rInt].positions;
+    }
+
+    private void SpeedUp()
+    {
+        //Determine max speed, and don't go past it
+        //Lower speedups as speed gets really fast
+        if (newBlockCreationDelay > 2) newBlockCreationDelay -= relNewBlock;
+        if (obstacleSpeed < 40f) obstacleSpeed += relMaxSpeed;
+        print(newBlockCreationDelay);
+        print(obstacleSpeed);
+    }
+}
+
+enum Modes
+{
+    Endless,
+    Standard
 }
 
 abstract class Obstacle 
@@ -53,72 +125,72 @@ abstract class Obstacle
 
 class ObstacleRight : Obstacle
 {
-    public ObstacleRight(Vector3 offset)
+    public ObstacleRight(Vector3 offset, Vector3 scale)
     {
         positions = new Vector3[3];
-        positions[0] = offset + new Vector3(1, 2, 0);
-        positions[1] = offset + new Vector3(1, 1, 0);
-        positions[2] = offset + new Vector3(1, 0, 0);
+        positions[0] = offset + new Vector3(1*scale.x, 2*scale.y, 0);
+        positions[1] = offset + new Vector3(1*scale.x, 1*scale.y, 0);
+        positions[2] = offset + new Vector3(1*scale.x, 0, 0);
     }
 }
 
 class ObstacleVRight : Obstacle
 {
-    public ObstacleVRight(Vector3 offset)
+    public ObstacleVRight(Vector3 offset, Vector3 scale)
     {
         positions = new Vector3[6];
-        positions[0] = offset + new Vector3(0, 2, 0);
-        positions[1] = offset + new Vector3(0, 1, 0);
+        positions[0] = offset + new Vector3(0, 2*scale.y, 0);
+        positions[1] = offset + new Vector3(0, 1*scale.y, 0);
         positions[2] = offset + new Vector3(0, 0, 0);
-        positions[3] = offset + new Vector3(1, 2, 0);
-        positions[4] = offset + new Vector3(1, 1, 0);
-        positions[5] = offset + new Vector3(1, 0, 0);
+        positions[3] = offset + new Vector3(1*scale.x, 2*scale.y, 0);
+        positions[4] = offset + new Vector3(1*scale.x, 1*scale.y, 0);
+        positions[5] = offset + new Vector3(1*scale.x, 0, 0);
     }
 }
 
 class ObstacleLeft : Obstacle
 {
-    public ObstacleLeft(Vector3 offset)
+    public ObstacleLeft(Vector3 offset, Vector3 scale)
     {
         positions = new Vector3[3];
-        positions[0] = offset + new Vector3(-1, 2, 0);
-        positions[1] = offset + new Vector3(-1, 1, 0);
-        positions[2] = offset + new Vector3(-1, 0, 0);
+        positions[0] = offset + new Vector3(-1*scale.x, 2*scale.y, 0);
+        positions[1] = offset + new Vector3(-1*scale.x, 1*scale.y, 0);
+        positions[2] = offset + new Vector3(-1*scale.x, 0, 0);
     }
 }
 
 class ObstacleVLeft : Obstacle
 {
-    public ObstacleVLeft(Vector3 offset)
+    public ObstacleVLeft(Vector3 offset, Vector3 scale)
     {
         positions = new Vector3[6];
-        positions[0] = offset + new Vector3(0, 2, 0);
-        positions[1] = offset + new Vector3(0, 1, 0);
+        positions[0] = offset + new Vector3(0, 2*scale.y, 0);
+        positions[1] = offset + new Vector3(0, 1*scale.y, 0);
         positions[2] = offset + new Vector3(0, 0, 0);
-        positions[3] = offset + new Vector3(-1, 2, 0);
-        positions[4] = offset + new Vector3(-1, 1, 0);
-        positions[5] = offset + new Vector3(-1, 0, 0);
+        positions[3] = offset + new Vector3(-1*scale.x, 2*scale.y, 0);
+        positions[4] = offset + new Vector3(-1*scale.x, 1*scale.y, 0);
+        positions[5] = offset + new Vector3(-1*scale.x, 0, 0);
     }
 }
 
 class ObstacleDown : Obstacle
 {
-    public ObstacleDown(Vector3 offset)
+    public ObstacleDown(Vector3 offset, Vector3 scale)
     {
         positions = new Vector3[3];
-        positions[0] = offset + new Vector3(-1, 0, 0);
+        positions[0] = offset + new Vector3(-1*scale.x, 0, 0);
         positions[1] = offset + new Vector3(0, 0, 0);
-        positions[2] = offset + new Vector3(1, 0, 0);
+        positions[2] = offset + new Vector3(1*scale.x, 0, 0);
     }
 }
 
 class ObstacleUp : Obstacle
 {
-    public ObstacleUp(Vector3 offset)
+    public ObstacleUp(Vector3 offset, Vector3 scale)
     {
         positions = new Vector3[3];
-        positions[0] = offset + new Vector3(-1, 2, 0);
-        positions[1] = offset + new Vector3(0, 2, 0);
-        positions[2] = offset + new Vector3(1, 2, 0);
+        positions[0] = offset + new Vector3(-1*scale.x, 2*scale.y, 0);
+        positions[1] = offset + new Vector3(0, 2*scale.y, 0);
+        positions[2] = offset + new Vector3(1*scale.x, 2*scale.y, 0);
     }
 }
--- gameDev/moveBlock.cs
+++ gameDev/moveBlock.cs
@@ -2,23 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class moveBlock : MonoBehaviour
+public class MoveBlock : MonoBehaviour
 {
     public float deleteDepth = -5.0f;
-    public float speed = 0.001f;
+    Points myPoints;
+    DynamicTest myObstacles;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myPoints = GameObject.Find("Points").GetComponent<Points>();
+        myObstacles = GameObject.Find("ObstacleGeneration").GetComponent<DynamicTest>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position -= new Vector3(0, 0, speed);
+        float speed = myObstacles.obstacleSpeed;
+        transform.position -= new Vector3(0, 0, speed*Time.deltaTime);
 
         if (transform.position.z <= deleteDepth)
         {
+            myPoints.AddPoints();
             Destroy(gameObject);
         }
     }

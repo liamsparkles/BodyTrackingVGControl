6a987f69573823d6249967a6d5e855877f0c59e0
Liam Sparling <liam.sparling@ieee.org>
2021-12-11 22:43:41 -0600

    Added the dynamic test with jumping

--- DynamicTest.cs
+++ DynamicTest.cs
@@ -1,55 +1,47 @@
 using UnityEngine;
 public class DynamicTest : MonoBehaviour
 {
-    private Obstacle[] obstacles = new Obstacle[5];
+    private Obstacle[] obstacles = new Obstacle[6];
 
     // Instantiates prefabs in a circle formation
-    public float DEFAULTCREATIONDELAY = 4;
-    public float DEFAULTOBSTACLESPEED = 2;
-
-    public GameObject prefab;
     private double newBlockTime;
-    private float newBlockCreationDelay;
+    public float newBlockCreationDelay = 5;
+    public GameObject prefabGeneral;
+    public GameObject prefabJump;
     //private int curState;
     public float dist = 20;
-    public Modes gameMode = Modes.Waiting;
+    Modes gameMode = Modes.Endless;
     private int[] standardGame;
     private int curObstacle;
-    public float obstacleSpeed;
+    public float obstacleSpeed = 10f;
     private float relMaxSpeed;
     private float relNewBlock;
-    public float offsety;
-    //Points myPoints;
+    Points myPoints;
 
     void Start()
     {
-        Vector3 scale = prefab.transform.localScale;
-        offsety = scale.y / 2;
-        Vector3 offset = transform.position + new Vector3(0, scale.y/2, dist);
-        obstacles[0] = new ObstacleLeft(offset, scale);
-        obstacles[1] = new ObstacleVLeft(offset, scale);
-        obstacles[2] = new ObstacleRight(offset, scale);
-        obstacles[3] = new ObstacleVRight(offset, scale);
-        //obstacles[4] = new ObstacleDown(offset, scale);
-        obstacles[4] = new ObstacleUp(offset, scale);
+        Vector3 offset = transform.position + new Vector3(0, 0, dist);
+        obstacles[0] = new ObstacleLeft(offset, prefabGeneral);
+        obstacles[1] = new ObstacleVLeft(offset, prefabGeneral);
+        obstacles[2] = new ObstacleRight(offset, prefabGeneral);
+        obstacles[3] = new ObstacleVRight(offset, prefabGeneral);
+        obstacles[4] = new ObstacleDown(offset, prefabJump);
+        obstacles[5] = new ObstacleUp(offset, prefabGeneral);
 
         newBlockTime = 0;
 
-        standardGame = new int[]
-        {
-            1, 2, 3, 4, 0,
-            3, 4, 0, 3, 4, 1, 3,
-            3, 3, 1, 1, 2, 0, 4, 3, 1, 4
+        standardGame = new int[] 
+        { 
+            1, 2, 3, 4, 5, 0,
+            3, 4, 5, 0, 3, 5, 5, 4, 1, 3,
+            3, 3, 1, 1, 2, 0, 5, 4, 3, 5, 1, 4
         };
         curObstacle = 0;
 
-        //myPoints = GameObject.Find("Score").GetComponent<Points>();
+        myPoints = GameObject.Find("Points").GetComponent<Points>();
 
-        obstacleSpeed = DEFAULTOBSTACLESPEED;
-        newBlockCreationDelay = DEFAULTCREATIONDELAY;
-        relMaxSpeed = obstacleSpeed * 0.5f;
-        relNewBlock = 0.1F;
-        
+        relMaxSpeed = obstacleSpeed * 0.1f;
+        relNewBlock = 0.025f;
         //Vector3 pos = transform.position + new Vector3(x, y, z);
 
         //For the move left obstacle
@@ -62,8 +54,7 @@ public class DynamicTest : MonoBehaviour
         newBlockTime -= Time.deltaTime;
         if (newBlockTime <= 0)
         {
-            newBlockTime = newBlockCreationDelay;
-            Vector3[] curPos;
+            Obstacle myObst;
             if (gameMode == Modes.Standard)
             {
                 if (curObstacle >= standardGame.Length)
@@ -76,43 +67,38 @@ public class DynamicTest : MonoBehaviour
                     }
                     return;
                 }
-                curPos = standardMode();
+                myObst = standardMode();
             }
             else if (gameMode == Modes.Endless)
-            {
-                curPos = endlessMode();
-            }
-            else if (gameMode == Modes.Waiting)
-            {
-                //print("here");
-                return;
+            { 
+                myObst = endlessMode();
             }
             else
             {
+                myObst = obstacles[0];
                 print("error");
-                return;
             }
 
-            for (int i = 0; i < curPos.Length; i++)
+            for (int i = 0; i < myObst.positions.Length; i++)
             {
-                Instantiate(prefab, curPos[i], Quaternion.identity);
+                Instantiate(myObst.prefab, myObst.positions[i], Quaternion.identity); 
             }
-
+            newBlockTime = newBlockCreationDelay;
         }
     }
 
-    private Vector3[] standardMode()
+    private Obstacle standardMode()
     {
         SpeedUp();
         SpeedUp();
-        return obstacles[standardGame[curObstacle++]].positions;
-    }
+        return obstacles[standardGame[curObstacle++]];
+    } 
 
-    private Vector3[] endlessMode()
+    private Obstacle endlessMode()
     {
         SpeedUp();
         int rInt = (int)Random.Range(0, obstacles.Length - 0.001f);
-        return obstacles[rInt].positions;
+        return obstacles[rInt];
     }
 
     private void SpeedUp()
@@ -121,97 +107,103 @@ public class DynamicTest : MonoBehaviour
         //Lower speedups as speed gets really fast
         if (newBlockCreationDelay > 2) newBlockCreationDelay -= relNewBlock;
         if (obstacleSpeed < 40f) obstacleSpeed += relMaxSpeed;
-        //print(newBlockCreationDelay);
-        //print(obstacleSpeed);
-    }
-
-    public void ResetSpeeds()
-    {
-        obstacleSpeed = DEFAULTOBSTACLESPEED;
-        newBlockCreationDelay = DEFAULTCREATIONDELAY;
+        print(newBlockCreationDelay);
+        print(obstacleSpeed);
     }
 }
 
-public enum Modes
+enum Modes
 {
     Endless,
-    Standard,
-    Waiting
+    Standard
 }
 
-abstract class Obstacle
+abstract class Obstacle 
 {
+    public GameObject prefab;
     public Vector3[] positions;
 }
 
 class ObstacleRight : Obstacle
 {
-    public ObstacleRight(Vector3 offset, Vector3 scale)
+    public ObstacleRight(Vector3 offset, GameObject myprefab)
     {
+        prefab = myprefab;
+        Vector3 scale = prefab.transform.localScale;
         positions = new Vector3[3];
-        positions[0] = offset + new Vector3(1 * scale.x, 2 * scale.y, 0);
-        positions[1] = offset + new Vector3(1 * scale.x, 1 * scale.y, 0);
-        positions[2] = offset + new Vector3(1 * scale.x, 0, 0);
+        positions[0] = offset + new Vector3(1*scale.x, 2*scale.y, 0);
+        positions[1] = offset + new Vector3(1*scale.x, 1*scale.y, 0);
+        positions[2] = offset + new Vector3(1*scale.x, 0, 0);
     }
 }
 
 class ObstacleVRight : Obstacle
 {
-    public ObstacleVRight(Vector3 offset, Vector3 scale)
+    public ObstacleVRight(Vector3 offset, GameObject myprefab)
     {
+        prefab = myprefab;
+        Vector3 scale = prefab.transform.localScale;
         positions = new Vector3[6];
-        positions[0] = offset + new Vector3(0, 2 * scale.y, 0);
-        positions[1] = offset + new Vector3(0, 1 * scale.y, 0);
+        positions[0] = offset + new Vector3(0, 2*scale.y, 0);
+        positions[1] = offset + new Vector3(0, 1*scale.y, 0);
         positions[2] = offset + new Vector3(0, 0, 0);
-        positions[3] = offset + new Vector3(1 * scale.x, 2 * scale.y, 0);
-        positions[4] = offset + new Vector3(1 * scale.x, 1 * scale.y, 0);
-        positions[5] = offset + new Vector3(1 * scale.x, 0, 0);
+        positions[3] = offset + new Vector3(1*scale.x, 2*scale.y, 0);
+        positions[4] = offset + new Vector3(1*scale.x, 1*scale.y, 0);
+        positions[5] = offset + new Vector3(1*scale.x, 0, 0);
     }
 }
 
 class ObstacleLeft : Obstacle
 {
-    public ObstacleLeft(Vector3 offset, Vector3 scale)
+    public ObstacleLeft(Vector3 offset, GameObject myprefab)
     {
+        prefab = myprefab;
+        Vector3 scale = prefab.transform.localScale;
         positions = new Vector3[3];
-        positions[0] = offset + new Vector3(-1 * scale.x, 2 * scale.y, 0);
-        positions[1] = offset + new Vector3(-1 * scale.x, 1 * scale.y, 0);
-        positions[2] = offset + new Vector3(-1 * scale.x, 0, 0);
+        positions[0] = offset + new Vector3(-1*scale.x, 2*scale.y, 0);
+        positions[1] = offset + new Vector3(-1*scale.x, 1*scale.y, 0);
+        positions[2] = offset + new Vector3(-1*scale.x, 0, 0);
     }
 }
 
 class ObstacleVLeft : Obstacle
 {
-    public ObstacleVLeft(Vector3 offset, Vector3 scale)
+    public ObstacleVLeft(Vector3 offset, GameObject myprefab)
     {
+        prefab = myprefab;
+        Vector3 scale = prefab.transform.localScale;
         positions = new Vector3[6];
-        positions[0] = offset + new Vector3(0, 2 * scale.y, 0);
-        positions[1] = offset + new Vector3(0, 1 * scale.y, 0);
+        positions[0] = offset + new Vector3(0, 2*scale.y, 0);
+        positions[1] = offset + new Vector3(0, 1*scale.y, 0);
         positions[2] = offset + new Vector3(0, 0, 0);
-        positions[3] = offset + new Vector3(-1 * scale.x, 2 * scale.y, 0);
-        positions[4] = offset + new Vector3(-1 * scale.x, 1 * scale.y, 0);
-        positions[5] = offset + new Vector3(-1 * scale.x, 0, 0);
+        positions[3] = offset + new Vector3(-1*scale.x, 2*scale.y, 0);
+        positions[4] = offset + new Vector3(-1*scale.x, 1*scale.y, 0);
+        positions[5] = offset + new Vector3(-1*scale.x, 0, 0);
     }
 }
 
 class ObstacleDown : Obstacle
 {
-    public ObstacleDown(Vector3 offset, Vector3 scale)
+    public ObstacleDown(Vector3 offset, GameObject myprefab)
     {
+        prefab = myprefab;
+        Vector3 scale = prefab.transform.localScale;
         positions = new Vector3[3];
-        positions[0] = offset + new Vector3(-1 * scale.x, 0, 0);
+        positions[0] = offset + new Vector3(-1*scale.x, 0, 0);
         positions[1] = offset + new Vector3(0, 0, 0);
-        positions[2] = offset + new Vector3(1 * scale.x, 0, 0);
+        positions[2] = offset + new Vector3(1*scale.x, 0, 0);
     }
 }
 
 class ObstacleUp : Obstacle
 {
-    public ObstacleUp(Vector3 offset, Vector3 scale)
+    public ObstacleUp(Vector3 offset, GameObject myprefab)
     {
+        prefab = myprefab;
+        Vector3 scale = prefab.transform.localScale;
         positions = new Vector3[3];
-        positions[0] = offset + new Vector3(-1 * scale.x, 2 * scale.y, 0);
-        positions[1] = offset + new Vector3(0, 2 * scale.y, 0);
-        positions[2] = offset + new Vector3(1 * scale.x, 2 * scale.y, 0);
+        positions[0] = offset + new Vector3(-1*scale.x, 2*scale.y, 0);
+        positions[1] = offset + new Vector3(0, 2*scale.y, 0);
+        positions[2] = offset + new Vector3(1*scale.x, 2*scale.y, 0);
     }
 }
